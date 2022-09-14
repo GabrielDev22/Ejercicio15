f25867750b39933f22b45559b301c43678a598be
Emanuel Some <intercab@intraudesa.edu.ar>
2022-09-14 19:04:55 -0300

    Agrego funcion tablaDelSeis

--- js/ejercicio15.js
+++ js/ejercicio15.js
@@ -259,7 +259,18 @@ function tablaDelSeis() {
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
+
+  let vectorDelSeis = [] 
+ 
+  for (let i = 0; i < 11; i++) {
+      vectorDelSeis.push(i*6)
+  }
+
+  return vectorDelSeis 
 }
+
+console.log(tablaDelSeis())
+
 
 function tieneTresDigitos(numero) {
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
